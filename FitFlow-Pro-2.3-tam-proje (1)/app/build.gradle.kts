4b0fba488bf3f1ed1a77046db776ddf2360d89ca
nesimik <35139384+nesimik@users.noreply.github.com>
2026-09-25 14:29:28 +0300

    Update build.gradle.kts

--- FitFlow-Pro-2.3-tam-proje (1)/app/build.gradle.kts
+++ FitFlow-Pro-2.3-tam-proje (1)/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.nesimi.fitflow3.1"
+    applicationId = "com.nesimi.fitflow2"
     minSdk = 24
     targetSdk = 36
     versionCode = 3
